e7258acf90976cf1ab576028d002800a1846ec13
Ashmika Hirani <ashmika@factorial.io>
2019-12-29 03:31:32 -0500

    Add MaintenanceModeCollector as Maintenance mode monitoring collector plugin

---
+++ src/Plugin/MonitoringCollector/MaintenanceModeCollector.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\factorial_monitoring_connector\Plugin\MonitoringCollector;
+
+use Drupal\factorial_monitoring_connector\MonitoringCollectorPluginBase;
+use Drupal\factorial_monitoring_connector\MonitoringCollectorPluginInterface;
+
+/**
+ * Maintenance mode collector.
+ *
+ * @MonitoringCollector(
+ *   id = "maintenancemode",
+ *   label = @Translation("Maintenance mode monitoring collector plugin.")
+ * )
+ */
+class MaintenanceModeCollector extends MonitoringCollectorPluginBase implements MonitoringCollectorPluginInterface
+{
+    /**
+     * Collects data.
+     *
+     * @return array
+     *   Collected data.
+     */
+    public function collect() {
+        $maintenance_mode = \Drupal::state()->get('system.maintenance_mode',FALSE);
+        $msg = $maintenance_mode ? \Drupal::state()->get('system.maintenance_mode_message',NULL) : NULL;
+        return array(
+            array(
+            'group' => 'core',
+            'type' => $maintenance_mode ? 'error' : 'status',
+            'name' => 'Maintenance mode',
+            'description' => $msg,
+            'value_type' => 'boolean',
+            'value' => $maintenance_mode,
+            )
+        );
+    }
+}
